8970f6d39cbbdd364c846cf3e32a23a30d9d0059
6lv1n <sylvaptik@gmail.com>
2010-12-04 18:34:30 +0100

    New sources more stable... but not yet stable enought

--- src/com/travelreminder/android22/TravelReminder.java
+++ src/com/travelreminder/android22/TravelReminder.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.travelreminder.android22.MyLocation.LocationResult;
 
 public class TravelReminder extends Activity {
