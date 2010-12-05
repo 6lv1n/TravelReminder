3f8b81592d1e74b9406978cc27a8d25cc05e98e8
6lv1n <sylvaptik@gmail.com>
2010-12-05 15:46:15 +0100

    no comment

--- src/com/travelreminder/android22/TravelReminder.java
+++ src/com/travelreminder/android22/TravelReminder.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-// Local import 
 import com.travelreminder.android22.MyLocation.LocationResult;
 
 public class TravelReminder extends Activity {
