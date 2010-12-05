e0a22aa3d59fef9ab6dca21f70024b82800ec542
6lv1n <sylvaptik@gmail.com>
2010-12-05 17:37:16 +0100

    format sources

--- src/com/travelreminder/android22/StepComparator.java
+++ src/com/travelreminder/android22/StepComparator.java
@@ -1,15 +1,12 @@
 package com.travelreminder.android22;
 
 import java.util.Comparator;
-
 import android.text.format.Time;
 
 public class StepComparator implements Comparator<Step> {
-
 	public int compare(Step object1, Step object2) {
-        final Step 	s0 		= object1;
-        final Step 	s1 		= object2;
-        return Time.compare(s0.getTime(), s1.getTime());
+        final 	Step 	s0	=	object1;
+        final 	Step 	s1	=	object2;
+        return 	Time.compare(s0.getTime(), s1.getTime());
 	}
-	
 }
