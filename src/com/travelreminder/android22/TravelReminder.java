b21c9ed8d9dab822ab1e1e03664f26c2927c83a5
6lv1n <sylvaptik@gmail.com>
2010-12-05 18:13:11 +0100

    GPS TIMEOUT INCREASED ERROR STATE IMPLEMENTED (WIP)

--- src/com/travelreminder/android22/TravelReminder.java
+++ src/com/travelreminder/android22/TravelReminder.java
@@ -15,11 +15,11 @@ public class TravelReminder extends Activity {
 
 	public static boolean TR_IS_RUNNING = false;
 
-	private TextView 	mTxtViewlong;
-	private TextView	mTxtViewlat;
-	private TextView 	mTxtViewtrav;
-	private Travel 		testTravel;
-	private MyLocation 	myLocation;
+	private TextView mTxtViewlong;
+	private TextView mTxtViewlat;
+	private TextView mTxtViewtrav;
+	private Travel testTravel;
+	private MyLocation myLocation;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -32,16 +32,18 @@ public class TravelReminder extends Activity {
 	}
 
 	public void startTravelButtonAction(View view) {
-		if(!TR_IS_RUNNING) {
+		if (!TR_IS_RUNNING) {
 			String txtToast = "TR started!";
-			Toast toast = Toast.makeText(getApplicationContext(), txtToast, Toast.LENGTH_SHORT);
+			Toast toast = Toast.makeText(getApplicationContext(), txtToast,
+					Toast.LENGTH_SHORT);
 			toast.show();
 			testTravel = new Travel();
 			myLocation = new MyLocation();
 			TR_IS_RUNNING = true;
 		} else {
 			String txtToast = "TR is not started!";
-			Toast toast = Toast.makeText(getApplicationContext(), txtToast, Toast.LENGTH_SHORT);
+			Toast toast = Toast.makeText(getApplicationContext(), txtToast,
+					Toast.LENGTH_SHORT);
 			toast.show();
 			testTravel = null;
 			TR_IS_RUNNING = false;
@@ -49,34 +51,41 @@ public class TravelReminder extends Activity {
 	}
 
 	public void addStepButtonAction(View view) {
-		if(!TR_IS_RUNNING) {
+		if (!TR_IS_RUNNING) {
 			String txtToast = "TR is not started!";
-			Toast toast = Toast.makeText(getApplicationContext(), txtToast, Toast.LENGTH_SHORT);
+			Toast toast = Toast.makeText(getApplicationContext(), txtToast,
+					Toast.LENGTH_SHORT);
 			toast.show();
 		} else {
 			getUserPosition();
 		}
 	}
-	
+
 	public void exitButtonAction(View view) {
 		finish();
 	}
-	
+
 	public void getUserPosition() {
-		myLocation.getLocation(this, locationResult);
+		if(!myLocation.getLocation(this, locationResult)) {
+			String txtToast = "BUGGG 123";
+			Toast toast = Toast.makeText(getApplicationContext(), txtToast,
+					Toast.LENGTH_SHORT);
+			toast.show();			
+		} 
 	}
 
 	public LocationResult locationResult = new LocationResult() {
 		@Override
 		public void gotLocation(final Location location) {
-			if(location != null) {
+			if (location != null) {
 				testTravel.addStep(location);
-				mTxtViewlat.setText(" "	+ location.getLatitude());
+				mTxtViewlat.setText(" " + location.getLatitude());
 				mTxtViewlong.setText(" " + location.getLongitude());
-				mTxtViewtrav.setText(" " + testTravel.getStep(location).toString());
+				mTxtViewtrav.setText(" "
+						+ testTravel.getStep(location).toString());
 			} else {
 				mTxtViewlat.setText("Bordel !");
 			}
-		} 
+		}
 	};
 }
