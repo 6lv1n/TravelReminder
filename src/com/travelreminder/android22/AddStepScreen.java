f07a65c3787f632a5445cfe26675fda878a1e77f
6lv1n <sylvaptik@gmail.com>
2010-12-06 18:41:28 +0100

    other big changes

---
+++ src/com/travelreminder/android22/AddStepScreen.java
@@ -0,0 +1,22 @@
+package com.travelreminder.android22;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddStepScreen extends Activity {
+
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.addStepScreen);
+		Button b = (Button) findViewById(R.id.btnClick2);
+		b.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
+
+	}
+}
