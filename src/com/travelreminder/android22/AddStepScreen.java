1bb4084b7a4effe82a497cd768e22b5e9c3840b0
6lv1n <sylvaptik@gmail.com>
2010-12-06 18:59:59 +0100

    new view for add step (wip)

--- src/com/travelreminder/android22/AddStepScreen.java
+++ src/com/travelreminder/android22/AddStepScreen.java
@@ -6,17 +6,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class AddStepScreen extends Activity {
-
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		setContentView(R.layout.addStepScreen);
-		Button b = (Button) findViewById(R.id.btnClick2);
-		b.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View arg0) {
-				setResult(RESULT_OK);
-				finish();
-			}
-		});
-
-	}
+   public void onCreate(Bundle icicle) {
+      super.onCreate(icicle);
+      setContentView(R.layout.addstepscreen);
+      Button b = (Button) findViewById(R.id.btnClick2);
+      b.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View arg0) {
+         setResult(RESULT_OK);
+         finish();
+         }
+      });
+   }
 }
